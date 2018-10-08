c40f822209ee7d8c04daec73c88d33aed2bc9825
Talgat <tairovtalgat@gmail.com>
2018-10-08 21:54:01 +0600

    fix deleting, add test template func

--- main.cpp
+++ main.cpp
@@ -5,12 +5,36 @@
 
 using namespace std;
 
+template<typename T = int, int dmns3, int dmns2, int dmns1>
+void test(T testArr[dmns3][dmns2][dmns1], T*** src)
+{
+	cout << "\n************************** TEST *******************************\n";
+	for (int i = 0; i < dmns3; i++)
+	{
+		for (int j = 0; j < dmns2; j++)
+		{
+			for (int k = 0; k < dmns1; k++)
+			{
+				int num = testArr[i][j][k] = src[i][j][k];
+				cout << num << '\t';
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << "************************** TEST *******************************\n";
+
+}
+
+
+
 int main()
 {
 	srand(time(nullptr));
 
-	int masA = 3, masB = 4, masC = 5;
+	const int masA = 3, masB = 4, masC = 5;
 	int*** cr, **ir, *jr;
+
 
 	////объявление 
 	int*** arr = new int**[masA];
@@ -36,16 +60,13 @@ int main()
 		}
 		cout << endl;
 	}
-	//////////////////////////////////// очистка
-	for (cr = arr; cr < arr + masA; cr++)     // надо посмотреть с циклом while или присваивание ук. сделать вне цикла
-	{
-		for (ir = *cr; ir < *cr + masB; ir++)
-		{
-			delete[] * ir;
-			*ir = nullptr;
-		}
-	}
 
+	
+	int testArr[masA][masB][masC];
+	test<int, masA, masB, masC>(testArr, arr);
+
+
+	//////////////////////////////////// очистка
 	for (cr = arr; cr < arr + masA; cr++)
 	{
 		for (ir = *cr; ir < *cr + masB; ir++)
@@ -54,9 +75,9 @@ int main()
 		}
 		delete[] * cr;
 	}
-
 	delete[] arr;
-	arr = nullptr;
+
+	cout << "OK\n";
 
 	return 0;
 }
